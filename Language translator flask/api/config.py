b788ebab2a45e9f55ba29a3695dde94a9b648678
AngelaKasundi2001 <angela.20200981@iit.ac.lk>
2023-01-26 00:18:42 +0530

    first commit

---
+++ Language translator flask/api/config.py
@@ -0,0 +1,21 @@
+HUGGINGFACE_S3_BASE_URL="https://s3.amazonaws.com/models.huggingface.co/bert/Helsinki-NLP"
+FILENAMES = ["config.json","pytorch_model.bin","source.spm","target.spm","tokenizer_config.json","vocab.json"]
+MODEL_PATH = "data"
+
+SUPPORTED_LANGUAGES = [
+        'English',
+        'Hindi',
+        'Japanese',
+        'Chinese',
+        'German',
+        'French',
+        'Arabic',
+        'Persian',
+        'Hebrew',
+        'Greek',
+        'Russian',
+        'Spanish',
+        'Italian',
+        'Tamil',
+        'Kannada',
+]
